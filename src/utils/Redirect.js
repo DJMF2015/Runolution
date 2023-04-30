6155996f2f9ea1f92a88517a6a4eb3ed1ae0225b
DJMF2015 <djm.fulton@gmail.com>
2023-04-30 18:30:11 +0100

    refactor of helper function to fetch payload and added function to check expiry date of access token

--- src/utils/Redirect.js
+++ src/utils/Redirect.js
@@ -1,38 +1,40 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getExchangeCodeFromURL, getAccessToken } from './functions';
-// create context to store payload data
 const Redirect = () => {
   const navigate = useNavigate();
-  const [authCode, setAuthToken] = useState('');
   const [payload, setPayload] = useState([]);
   useEffect(() => {
     async function getToken() {
-      const urlLocation = new URLSearchParams(window.location.search).get('code');
-      console.log('urlLocation', urlLocation);
-      const data = await getExchangeCodeFromURL(urlLocation);
-      setAuthToken(data);
-
-      const response = await getAccessToken(data);
-      console.log('response', response);
+      const exchangeCode = new URLSearchParams(window.location.search).get('code');
+      const code = await getExchangeCodeFromURL(exchangeCode);
+      const response = await getAccessToken(code);
       setPayload(response);
     }
 
     getToken();
   }, [payload]);
-  console.log('payload', payload);
 
-  // store the payload from useeffect getToken() function into local storage and ContextProvider
+  // check of access token has expired and if so then redirect to login page to get a new token from Strava
+  useEffect(() => {
+    const checkIfTokenExpired = () => {
+      const expires_at = localStorage.getItem('expires_at');
+      const expires_in = localStorage.getItem('expires_in');
+      if (expires_in && expires_at) {
+        const timeElapsed = Date.now() - expires_at;
+        if (timeElapsed / 1000 > expires_in) {
+          // if time elapsed is greater than expires_in then token has expired
+          navigate('/login');
+        }
+      }
+    };
+    checkIfTokenExpired();
+  });
+
   useEffect(() => {
     if (payload && payload?.access_token) {
-      localStorage.setItem('payload', JSON.stringify(payload));
-      localStorage.setItem('access_token', JSON.stringify(payload.access_token));
-      localStorage.setItem('refresh_token', JSON.stringify(payload.refresh_token));
-      localStorage.setItem('expires_in', JSON.stringify(payload.expires_in));
-      localStorage.setItem('athlete_id', JSON.stringify(payload.athlete.id));
-      navigate('/', { state: { payload: payload } });
+      navigate('/');
     }
-  }, [payload]);
-  return <div>Redirecting...</div>;
+  }, [payload, navigate]);
 };
 export default Redirect;
