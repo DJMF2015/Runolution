afbb8e72fffab6b99f304b5ea0781226e8ea2ea6
DJMF2015 <djm.fulton@gmail.com>
2023-05-10 16:46:50 +0100

    moved athlete stats to landing page and removed profile page

--- src/App.js
+++ src/App.js
@@ -5,24 +5,23 @@ import Layout from './pages/Layout';
 import Activities from './pages/AthleteActivities';
 import AthleteStats from './pages/AthleteStats';
 import AthleteClub from './pages/AthleteClub';
+import ActivityList from './components/ActivityList';
 import Activity from './components/ActivityCard';
 import Redirect from './utils/redirect';
-import Profile from './pages/Profile';
 import Login from './pages/Login';
 import ActivitiesMap from './pages/ActivitiesMap';
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Profile />} />
+            <Route path="/" element={<AthleteStats />} />
             <Route path="/login" element={<Login />} />
             <Route path="/redirect" element={<Redirect />} />
-            <Route path="/statistics" element={<AthleteStats />} />
             <Route path="/activity" element={<Activity />} />
             <Route path="/activities" element={<Activities />} />
+            <Route path="/splits" element={<ActivityList />} />
             <Route path="/map" element={<ActivitiesMap />} />
             <Route path="/club" element={<AthleteClub />} />
           </Route>
