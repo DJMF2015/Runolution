ef285186f136e6c0c229a6be328e4c8c7ab6862b
DJMF2015 <djm.fulton@gmail.com>
2023-05-10 16:59:09 +0100

    add side navigation and styles

--- src/pages/AthleteActivities.js
+++ src/pages/AthleteActivities.js
@@ -1,26 +1,30 @@
-import React, { Suspense, useState, useEffect } from 'react';
+import React, { Suspense, useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { mediaQueries } from '../utils/mediaQueries';
 import { getAthleteActivities } from '../utils/functions';
 import Pagination from '../utils/pagination';
 import { catchErrors } from '../utils/helpers';
+import { ArrowUpCircleFill } from '@styled-icons/bootstrap/ArrowUpCircleFill';
 import { getKmsToMiles, getSecondstoMinutes, formattedDate } from '../utils/conversion';
 import DropDown from '../components/ActivityDropDown';
+import polyline from '@mapbox/polyline';
 import Login from './Login';
+import Profile from './AthleteStats';
 import Search from '../utils/search';
 import '../App.css';
-import polyline from '@mapbox/polyline';
 import { Link } from 'react-router-dom';
 
 const AthleteActivities = () => {
   const [payload, setPayload] = useState([]);
   const [activities, setActivities] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
   const [activityName, setActivityName] = useState([]);
   const [searchTxt, setSearchTxt] = useState('');
   const [pageIndex, setPageIndex] = useState(1);
   const [loading, setLoading] = useState(false);
   // const [selectedName, setFilteredName] = useState(null);
   const [nodes, setNodes] = useState([]);
+
   useEffect(() => {
     const token = localStorage.getItem('access_token');
     const accessToken = JSON.parse(token);
@@ -45,6 +49,7 @@ const AthleteActivities = () => {
       for (let i in stravaActivityResponse) {
         const activityName = stravaActivityResponse[i]?.name;
         let activity_polyline = stravaActivityResponse?.[i]?.map?.summary_polyline;
+
         setActivityName(activityName);
         polylines.push({
           activityPositions: polyline.decode(activity_polyline),
@@ -64,27 +69,71 @@ const AthleteActivities = () => {
   // } else {
   //   filteredName = activities;
   // }
+  // map activity coordinates to GeoJSON from [0][1] to [1][0]
+  // const mapCoordinates = nodes.flatMap((node) => node.activityPositions.coordinates);
+  // console.log({ mapCoordinates });
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set top coordinate to 0
+  // for smooth scrolling
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+  }, []);
 
   const filteredName = activities.filter((activity) => {
     return activity.name.toLowerCase().includes(searchTxt.toLowerCase());
   });
-  if (loading)
-    return (
-      <h2>
-        <Suspense>Fetching Activities...</Suspense>
-      </h2>
-    );
+
   if (!activities) return <Suspense fallback={<div>loading...</div>}></Suspense>;
   console.log(activities);
   return (
     <>
-      {!payload && <Login />}
-      <Search searchTxt={searchTxt} updateSearchTxt={setSearchTxt} />
-      {/* <DropDown setFilteredName={setFilteredName} result={activities} /> */}
-      <Pagination
+      {/* <Pagination
         pageIndex={pageIndex}
         onPageChange={(currentPage) => setPageIndex(currentPage)}
-      />
+      /> */}
+      {!payload && <Login />}
+      {payload && <Profile />}
+      {/* <DropDown setFilteredName={setFilteredName} result={activities} /> */}
+
+      {isVisible && (
+        <div onClick={scrollToTop}>
+          <ScrollToTop alt="Go to top"></ScrollToTop>
+        </div>
+      )}
+      <SideNavigation>
+        <Search searchTxt={searchTxt} updateSearchTxt={setSearchTxt} />
+
+        {filteredName.map((activity, i) => (
+          <>
+            <div key={i}>
+              <Link
+                style={{ color: 'white' }}
+                to="/activity"
+                state={{ from: activity }}
+                key={`${activity.id}--${activity.moving_time}--${activity.average_heartrate}`}
+              >
+                <h2>
+                  {i + 1}. {activity.name}
+                </h2>
+              </Link>
+            </div>
+          </>
+        ))}
+      </SideNavigation>
 
       <CardDetails>
         {payload &&
@@ -108,57 +157,121 @@ const AthleteActivities = () => {
             </>
           ))}
       </CardDetails>
-      <div></div>
     </>
   );
 };
-const CardDetails = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 15em 1fr;
-  grid-gap: 1rem;
-  position: relative;
-  color: black;
-  background-color: #f5f5f5;
-  font-size: 1rem;
-  font-family: Comic Sans MS;
-  font-weight: bold;
-  text-align: center;
-  margin: 15px 35px 5px 35px;
-  font-weight: 200;
-  padding: 5px;
+// sidenavigation bar for search and pagination
+const SideNavigation = styled.div`
+  height: 100%;
+  width: 250px;
+  font-size: 0.8rem;
 
-  ${mediaQueries('md')` 
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  justify-content: center; 
-   position: relative;
-   font-size:.8rem; 
-  
-`}
+  padding: 10px;
+  display: block;
+  position: fixed;
+  border-right: 3px solid grey;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  scroll-behavior: smooth;
+  padding-top: 20px;
+  overflow: auto;
+  background-color: #111;
+  opacity: 0.8;
+  color: white;
+
+  /* customise scrollbar for modern browser except firefox*/
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 10px;
+  }
+  ::-webkit-scollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+
+  ::-webkit-scrollbar-thumb:active {
+    background-color: #555;
+  }
+  ::-webkit-scrollbar-thumb:window-inactive {
+    background-color: #555;
+  }
+  ::-webkit-scrollbar-thumb:horizontal {
+    background-color: #555;
+  }
+  ::-webkit-scrollbar-thumb:vertical {
+    background-color: #555;
+  }
+
+  a {
+    padding: 2px 0px 0px 15px;
+    line-break: 2px;
+    margin-top: 2px;
+    text-decoration: none;
+    font-size: 10px;
+    color: white;
+    display: block;
+  }
+
+  a:hover {
+    color: white;
+    text-decoration: underline;
+  }
+`;
+
+const CardDetails = styled.div`
+  display: inline-flex;
+  flex-wrap: wrap;
+  margin-left: 250px;
+  justify-content: center;
+  max-width: 40vw;
+  /* margin-top: -20px; */
+  background-color: ghostwhite;
+  position: relative;
+  font-family: 'Roboto', sans-serif;
 `;
 
 const Cardborder = styled.div`
-  border: 2px solid black;
+  border: 2px solid #fff;
   border-radius: 5px;
   box-sizing: border-box;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  padding: 5px;
-  margin: 0px 50px 05px 50px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  padding: 2rem;
+  margin: 2rem;
+  width: 400px;
+  height: 300px;
+  font-family: comic sans ms;
+  background-color: #fff;
+  color: #333;
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-align: center;
+  opacity: 0.5;
+  transition: all 0.3s ease-in-out;
 
-  ${mediaQueries('md')`
-  border: 2px solid black;
-  padding: 5px;
-  margin: 0px 50px 05px 50px;
-  border-radius: 5px;
-  font-size:.8rem;
-  `}
-  /* hover */
   &:hover {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
     transform: scale(1.05);
-    transition: all 0.3s ease-in-out;
+    opacity: 1;
+    /* make card fade as scroll out of view */
   }
+`;
+
+const ScrollToTop = styled(ArrowUpCircleFill)`
+  height: 3em;
+  display: flex;
+  z-index: 1;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  position: fixed;
+  margin: 0px 10px 40px 95vw;
 `;
 
 export default AthleteActivities;
