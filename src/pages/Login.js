798d132a29a8e8f28917e243bee81ccdefc44fa2
DJMF2015 <djm.fulton@gmail.com>
2023-05-10 16:49:30 +0100

    added styling to login and remove logout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 const Login = () => {
   const redirectUrl = 'http://localhost:3000/redirect';
 
@@ -7,18 +8,47 @@ const Login = () => {
     window.location = `http://www.strava.com/oauth/authorize?client_id=27989&response_type=code&redirect_uri=${redirectUrl}&approval_prompt=force&scope=read_all,activity:read_all,profile:read_all`;
   };
 
-  const handleLogout = () => {
-    localStorage.clear();
-    // remove token from state
-    window.location.href = 'http://localhost:3000/login';
-  };
-
   return (
     <>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+      <StyledLoginContainer>
+        <StyledLoginButton className="strava-button" onClick={handleLogin}>
+          Login
+        </StyledLoginButton>
+      </StyledLoginContainer>
     </>
   );
 };
 
 export default Login;
+
+const StyledLoginContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f2f2;
+  gap: 1rem;
+  margin: 0 auto;
+  height: 100vh;
+`;
+
+const StyledLoginButton = styled.a`
+  display: inline-block;
+  min-width: 80px;
+  background-color: #fc5200;
+  height: 48px;
+  width: 6%;
+  text-align: center;
+  margin-top: -7em;
+  line-height: 48px;
+  padding: 6px 12px;
+  font-size: 12px;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: 700;
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    filter: brightness(1.1);
+  }
+`;
