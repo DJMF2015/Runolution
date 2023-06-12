9d4d261ca61c936da27f19f3d0aaef7c5526f184
DJMF2015 <djm.fulton@gmail.com>
2023-06-12 10:53:19 +0100

    moved Layout to top level route and removed unused routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Activities from './pages/AthleteActivities';
 import ActivityList from './components/ActivityList';
-import Activity from './components/ActivityCard';
 import Redirect from './utils/redirect';
 import Login from './pages/Login';
 import ActivitiesMap from './pages/ActivitiesMap';
+import ActivitiesCard from './components/ActivitiesCard';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Layout />
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Activities />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/redirect" element={<Redirect />} />
-            <Route path="/activity" element={<Activity />} />
-            <Route path="/splits" element={<ActivityList />} />
-            <Route path="/map" element={<ActivitiesMap />} />
-          </Route>
+          <Route path="/" element={<Activities />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/redirect" element={<Redirect />} />
+          <Route path="/activity" element={<ActivitiesCard />} />
+          <Route path="/splits" element={<ActivityList />} />
+          <Route path="/map" element={<ActivitiesMap />} />
         </Routes>
       </BrowserRouter>
     </div>
