9b7ef0f55c1158b753df69270b83e60e0d2b2547
DJMF2015 <djm.fulton@gmail.com>
2023-06-12 10:38:39 +0100

    removal of useScreenshot hook as no longer used. Add hook for detecting screen width change

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,26 +1,18 @@
-import * as htmlToImage from 'html-to-image';
-// Desc: custom hooks for the app
-const createFileName = (extension = '', ...names) => {
-  if (!extension) {
-    return '';
-  }
+import { useEffect, useState } from 'react';
 
-  return `${names.join('')}.${extension}`;
-};
+// custom hook for getting the window width
+export const useGetWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-// custom hook for taking screenshot of the node and downloading it
-export const useScreenShot = (node) => {
-  const takeScreenShot = async (node) => {
-    const dataURI = await htmlToImage.toJpeg(node);
-    return dataURI;
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
-  const download = (image, { name = 'img', extension = 'jpg' } = {}) => {
-    const a = document.createElement('a');
-    a.href = image;
-    a.download = createFileName(extension, name);
-    a.click();
-  };
+    window.addEventListener('resize', handleResize);
 
-  return { takeScreenShot, download };
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return { windowWidth };
 };
