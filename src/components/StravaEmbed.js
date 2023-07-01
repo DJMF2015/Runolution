2b71549c75aa25a93e7938adc91c11250b64a8f0
DJMF2015 <djm.fulton@gmail.com>
2023-07-01 17:06:53 +0100

    add strava embedded widget

---
+++ src/components/StravaEmbed.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+// import Styled from 'styled-components';
+//
+function StravaEmbed({ id }) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://strava-embeds.com/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <script src="https://strava-embeds.com/embed.js" async />
+      </Helmet>
+
+      <div
+        className="strava-embed-placeholder"
+        data-embed-type="activity"
+        data-embed-id={id}
+      />
+    </>
+  );
+}
+
+export default StravaEmbed;
