502dff50f0e00665e83fa8d819965f9bed8b1660
DJMF2015 <djm.fulton@gmail.com>
2023-07-01 17:15:30 +0100

    add useScroll hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -16,3 +16,31 @@ export const useGetWindowWidth = () => {
 
   return { windowWidth };
 };
+
+export const useScroll = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return { isVisible, scrollToTop };
+};
