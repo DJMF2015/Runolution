e9d77c0c563c8d7dfdee7e7cb2cb8ea5ae96a67b
DJMF2015 <djm.fulton@gmail.com>
2023-07-01 17:16:32 +0100

    remove unused layers for map

--- src/components/layers.js
+++ src/components/layers.js
@@ -11,18 +11,6 @@ const Layers = [
     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
   },
   {
-    name: 'Thunderforest',
-    attribution:
-      '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    url: `https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=d28e4b3c141d47fe9d0b7ebccfebf144`,
-  },
-  {
-    name: 'Light Grey',
-    attribution:
-      '&copy; <a href="http://www.thunderforest.com/">Light Grey</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    url: `http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}`,
-  },
-  {
     name: 'Satellite',
     attribution:
       '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
