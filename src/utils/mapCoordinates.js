4db89d317f1a31ea0f34deb3e05e31d9cf808cd5
DJMF2015 <djm.fulton@gmail.com>
2023-07-02 17:47:13 +0100

    add helper function to handle activities geojson

---
+++ src/utils/mapCoordinates.js
@@ -0,0 +1,24 @@
+import mapboxgl from 'mapbox-gl';
+import * as turf from '@turf/turf';
+
+const MapCoordinates = (activity_toGEOJSON) => {
+  console.log({ activity_toGEOJSON });
+  const mapCoordinates = activity_toGEOJSON?.coordinates.map((item) => {
+    return item.map((coords) => {
+      return coords;
+    });
+  });
+
+  const data = {
+    type: 'Feature',
+    properties: { name: 'activity' },
+
+    geometry: {
+      type: 'LineString',
+      coordinates: mapCoordinates,
+    },
+  };
+  return data;
+};
+
+export default MapCoordinates;
