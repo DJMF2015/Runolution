766bccd78a7316cd6a4138d1009a1bff1f64c22d
DJMF2015 <djm.fulton@gmail.com>
2023-07-02 17:45:43 +0100

    add fix to handle check for access token. Add refactor to search component and removed geojson helper to seperate file

--- src/pages/AthleteActivities.js
+++ src/pages/AthleteActivities.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { mediaQueries } from '../utils/mediaQueries';
+import '../styles/AthleteStyles.css';
 import { getAthleteActivities, getUsersDetails } from '../utils/functions';
-import { checkIfTokenExpired } from '../utils/helpers';
 import { useGetWindowWidth, useScroll } from '../utils/hooks';
-import { catchErrors } from '../utils/helpers';
+import { catchErrors, checkIfTokenExpired } from '../utils/helpers';
 import LoadingWheel from '../styles/Loading.module.css';
 import { ArrowUpCircleFill } from '@styled-icons/bootstrap/ArrowUpCircleFill';
+import { HandThumbsUpFill } from '@styled-icons/bootstrap/HandThumbsUpFill';
+import { CalendarDateFill } from '@styled-icons/bootstrap/CalendarDateFill';
+import { Activity } from '@styled-icons/evaicons-solid/Activity';
+import { Stopwatch } from '@styled-icons/boxicons-regular/Stopwatch';
 import { getKmsToMiles, getSecondstoMinutes, formattedDate } from '../utils/conversion';
 import polyline from '@mapbox/polyline';
 import Login from './Login';
@@ -18,7 +21,6 @@ import AthleteStats from './AthleteStats';
 const AthleteActivities = () => {
   const [payload, setPayload] = useState([]);
   const [activities, setActivities] = useState([]);
-  // const [isVisible, setIsVisible] = useState(false);
   const [activityName, setActivityName] = useState([]);
   const [searchTxt, setSearchTxt] = useState('');
   const [pageIndex, setPageIndex] = useState(1);
@@ -26,7 +28,6 @@ const AthleteActivities = () => {
   const [nodes, setNodes] = useState([]);
   const { windowWidth } = useGetWindowWidth();
   const { isVisible, scrollToTop } = useScroll();
-
   useEffect(() => {
     const token = localStorage.getItem('access_token');
     const accessToken = JSON.parse(token);
@@ -36,28 +37,25 @@ const AthleteActivities = () => {
   useEffect(() => {
     async function fetchData() {
       const user = await getUsersDetails(payload);
-      console.log({ user });
     }
-    fetchData();
+    if (payload) {
+      fetchData();
+    }
   }, [payload]);
-
-  // useEffect(() => {
-  //   checkIfTokenExpired();
-  // });
 
   const fetchData = async (pageIndex) => {
     setLoading(true);
     try {
       if (payload) {
-        const response = await getAthleteActivities(payload, 200, pageIndex); // fetch data from strava api
+        const response = await getAthleteActivities(payload, 200, pageIndex); // fetch data from Strava API
         if (response.data.length === 0) {
           localStorage.setItem('activities', JSON.stringify(activities));
           return;
         }
         const newData = response.data || [];
         setActivities((prevItems) => [...prevItems, ...newData]);
+        setPageIndex((prevPage) => prevPage + 1); // increment page index
       }
-      setPageIndex((prevPage) => prevPage + 1); // increment page index
     } catch (error) {
       catchErrors(error);
     } finally {
@@ -66,47 +64,57 @@ const AthleteActivities = () => {
   };
 
   useEffect(() => {
-    fetchData(pageIndex);
-  }, [payload, pageIndex]);
+    const expires_at = localStorage.getItem('expires_at');
+    const expires_in = localStorage.getItem('expires_in');
+    const fetchTokenInfo = async () => {
+      try {
+        if (expires_at && expires_in) {
+          checkIfTokenExpired(expires_in, expires_at);
+        }
+      } catch (error) {
+        console.error('Error fetching token info:', error);
+      }
+    };
+
+    fetchTokenInfo();
+  }, []);
 
   useEffect(() => {
     const data = localStorage.getItem('activities'); // get data from local storage
     if (data !== null && data !== undefined) {
       setActivities(JSON.parse(data));
     } else {
-      catchErrors(() => fetchData(pageIndex));
+      fetchData(pageIndex);
     }
-  }, [pageIndex, payload]);
+  }, [payload, pageIndex]);
 
   useEffect(() => {
-    setLoading(true);
-    const stravaActivityResponse = activities;
-    let polylines = [];
-    for (let i in stravaActivityResponse) {
-      const activityName = stravaActivityResponse[i]?.name;
-      let activity_polyline = stravaActivityResponse?.[i]?.map?.summary_polyline;
-      if (
-        !activities ||
-        stravaActivityResponse === undefined ||
-        stravaActivityResponse === null
-      ) {
-        setLoading(false);
+    if (activities.length > 0) {
+      setLoading(true);
+      const stravaActivityResponse = activities;
+      let polylines = [];
+      for (let i in stravaActivityResponse) {
+        const activityName = stravaActivityResponse[i]?.name;
+        let activity_polyline = stravaActivityResponse?.[i]?.map?.summary_polyline;
+        if (stravaActivityResponse === undefined || stravaActivityResponse === null) {
+          setLoading(false);
+        }
+        setActivityName(activityName);
+        polylines.push({
+          activityPositions: polyline.toGeoJSON(activity_polyline),
+          activityName: activityName,
+        });
       }
-      setActivityName(activityName);
-      polylines.push({
-        activityPositions: polyline.toGeoJSON(activity_polyline),
-        activityName: activityName,
-      });
+      setNodes(polylines);
+      setLoading(false);
     }
-    setNodes(polylines);
-    setLoading(false);
   }, [activities]);
 
   const filteredName = activities.filter((activity) => {
     return activity.name.toLowerCase().includes(searchTxt.toLowerCase());
   });
 
-  if (!activities || loading)
+  if (loading)
     return (
       <div style={{ body: 'black' }}>
         <h1 style={{ color: 'red', textAlign: 'center' }}>
@@ -115,29 +123,31 @@ const AthleteActivities = () => {
         </h1>
       </div>
     );
+
   return (
     <>
-      {!payload ? <Login /> : <>{/* <AthleteProfile /> */}</>}
+      {!payload ? <Login /> : <> </>}
 
       {isVisible && (
         <div onClick={scrollToTop}>
           <ScrollToTop alt="Go to top"></ScrollToTop>
         </div>
       )}
-      <SideNavigation>
+      {windowWidth >= 600 && (
         <Search
-          key={searchTxt}
           searchTxt={searchTxt}
           updateSearchTxt={setSearchTxt}
           placeholder="search activities..."
         />
+      )}
+      <AthleteStats />
+      <SideNavigation>
         <div>
           {filteredName.map((activity, i) => (
             <>
               {activity.map?.summary_polyline ? (
-                <div key={i}>
+                <div key={activity.id}>
                   <Link
-                    style={{ color: 'white' }}
                     to="/activity"
                     state={{ from: activity }}
                     key={`${activity.id}--${activity.moving_time}--${activity.average_heartrate}`}
@@ -158,7 +168,6 @@ const AthleteActivities = () => {
           ))}
         </div>
       </SideNavigation>
-
       {windowWidth < 600 && (
         <Search
           searchTxt={searchTxt}
@@ -166,41 +175,58 @@ const AthleteActivities = () => {
           placeholder={'Search Activities'}
         />
       )}
-      {/* <AthleteStats /> */}
+
       <CardDetails>
-        {filteredName.map((activity, i) => (
-          <>
-            {activity.map?.summary_polyline ? (
-              <Cardborder>
-                <div key={i}>
-                  <Link
-                    to="/activity"
-                    state={{ from: activity }}
-                    key={`${activity.id}--${activity.moving_time}--${activity.average_heartrate}`}
-                  >
-                    <h2>{activity.name}</h2>
-                  </Link>
-                  <p>{getKmsToMiles(activity.distance)}</p>
-                  <p>{getSecondstoMinutes(activity.moving_time)} </p>
-                  <p>Date: {formattedDate(activity.start_date)}</p>
-                  <p>kudos: {activity.kudos_count}</p>
-                </div>
-              </Cardborder>
-            ) : (
-              <div key={i}>
+        {filteredName
+          .map((activity, i) => (
+            <>
+              {activity.map?.summary_polyline ? (
                 <Cardborder>
                   <div key={i}>
-                    <h2>{activity?.name}</h2>
-                    <p>{getKmsToMiles(activity?.distance)}</p>
-                    <p>{getSecondstoMinutes(activity?.moving_time)} </p>
-                    <p>Date: {formattedDate(activity?.start_date)}</p>
-                    <p>kudos: {activity?.kudos_count}</p>
+                    <Link
+                      style={{ textDecoration: 'none' }}
+                      to="/activity"
+                      state={{ from: activity }}
+                      key={`${activity.id}--${activity.moving_time}--${activity.average_heartrate}`}
+                    >
+                      <ActivityName>{activity.name}</ActivityName>
+                    </Link>
+                    <Text>
+                      <p>
+                        <ActivityIcon /> Distance: {getKmsToMiles(activity.distance)}
+                      </p>
+                      <p>
+                        <StopWatchIcon />
+                        Time: {getSecondstoMinutes(activity.moving_time)}{' '}
+                      </p>
+                      <p>
+                        {' '}
+                        <CalendarIcon />
+                        {formattedDate(activity.start_date)}
+                      </p>
+                      <p>
+                        {' '}
+                        <ThumbUpIcon /> kudos: {activity.kudos_count}{' '}
+                      </p>
+                    </Text>
                   </div>
                 </Cardborder>
-              </div>
-            )}
-          </>
-        ))}
+              ) : (
+                <div key={i}>
+                  <Cardborder>
+                    <div key={i}>
+                      <h2>{activity?.name}</h2>
+                      <p>{getKmsToMiles(activity?.distance)}</p>
+                      <p>{getSecondstoMinutes(activity?.moving_time)} </p>
+                      <p>Date: {formattedDate(activity?.start_date)}</p>
+                      <p>kudos: {activity?.kudos_count}</p>
+                    </div>
+                  </Cardborder>
+                </div>
+              )}
+            </>
+          ))
+          .slice(0, pageIndex + 10)}
       </CardDetails>
     </>
   );
@@ -208,14 +234,14 @@ const AthleteActivities = () => {
 // sidenavigation bar for search and pagination
 const SideNavigation = styled.div`
   height: 100%;
-  width: 250px;
+  width: 240px;
   font-size: 0.8rem;
+  position: fixed;
   padding: 10px;
   display: block;
   position: fixed;
-  border-right: 3px solid grey;
   z-index: 1000;
-  top: calc(10% - 20px);
+  top: calc(10% - 5px);
   left: 0;
   scroll-behavior: smooth;
   padding-top: 20px;
@@ -275,15 +301,35 @@ const SideNavigation = styled.div`
   }
 `;
 
+const RightHandSideProfile = styled.div`
+  height: 100%;
+  width: calc(14% - 50px);
+  display: inline-block;
+  color: 'red';
+  text-align: cente;
+  justify-content: center;
+  font-size: 1rem;
+  /* background-color: #111; */
+  position: fixed;
+  border-left: 3px solid grey;
+  z-index: 1000;
+  top: calc(11vh - 14px);
+  right: 0;
+  scroll-behavior: smooth;
+  padding-top: 20px;
+  margin: 0 auto;
+`;
 const CardDetails = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
   margin-left: 250px;
   justify-content: center;
-  background-color: #111;
   position: relative;
+  background-color: #fff;
+  /* z-index: */
   font-family: 'Roboto', sans-serif;
+  text-align: left;
 
   @media screen and (max-width: 600px) {
     display: flex;
@@ -296,20 +342,19 @@ const CardDetails = styled.div`
 `;
 
 const Cardborder = styled.div`
-  border: 2px solid #fff;
+  border: 2px solid #111;
   border-radius: 5px;
   box-sizing: border-box;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  padding: 2rem;
-  margin: 2rem;
-  width: 400px;
-  height: 300px;
-  font-family: comic sans ms;
+  padding: 0rem;
+  margin: 1rem;
+  margin-left: 3rem;
+  width: calc((100% - 7rem));
+  text-align: left;
   background-color: white;
   color: #333;
-  font-size: 1.2rem;
+  font-size: 0.75rem;
   font-weight: 500;
-  text-align: center;
   opacity: 0.7;
   transition: all 0.3s ease-in-out;
 
@@ -321,6 +366,23 @@ const Cardborder = styled.div`
   }
 `;
 
+const Text = styled.div`
+  font-size: 0.75rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+const ActivityName = styled.h2`
+  font-size: 1rem;
+  display: flex;
+  flex-direction: row;
+  margin: 15px 0 0 0; // top right bottom left
+  text-align: center;
+  justify-content: center;
+  color: ${(props) => props.theme.colour.strava};
+  text-decoration: none;
+`;
 const ScrollToTop = styled(ArrowUpCircleFill)`
   height: 3em;
   color: ${(props) => props.theme.colour.strava};
@@ -333,4 +395,31 @@ const ScrollToTop = styled(ArrowUpCircleFill)`
   margin: 0px 10px 40px 90vw;
 `;
 
+const ActivityIcon = styled(Activity)`
+  height: 2.5em;
+  width: 2.5em;
+  color: ${(props) => props.theme.colour.strava};
+  margin: 0px 5px 0px 0px;
+`;
+
+const StopWatchIcon = styled(Stopwatch)`
+  margin: 0px 5px 0px 0px;
+  width: 2em;
+  width: 2em;
+  color: ${(props) => props.theme.colour.strava};
+`;
+
+const ThumbUpIcon = styled(HandThumbsUpFill)`
+  margin: 0px 5px 0px 0px;
+  width: 2em;
+  height: 2em;
+  color: ${(props) => props.theme.colour.strava};
+`;
+
+const CalendarIcon = styled(CalendarDateFill)`
+  margin: 0px 10px 0px 0px;
+  width: 2em;
+  height: 2em;
+  color: ${(props) => props.theme.colour.strava};
+`;
 export default AthleteActivities;
