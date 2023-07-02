3e77969c610665c7f6b73845d6437634e807d9fb
DJMF2015 <djm.fulton@gmail.com>
2023-07-02 17:53:31 +0100

    added comment to file

--- src/components/ActivityDropDown.js
+++ src/components/ActivityDropDown.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+// NOTE: This component is not currently in use. It was an attempt to create a dropdown menu to filter activities by type. It is not currently in use because the Strava API does not provide a way to filter activities by type.
 export default function ActivityDropDown(props) {
   const [activities, setActivityName] = useState([]);
   const styles = {
