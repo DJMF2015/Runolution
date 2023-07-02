0efdc1f5d4530ea27589199a5d626b31f04ee271
DJMF2015 <djm.fulton@gmail.com>
2023-07-02 18:14:24 +0100

    renamed redirect to Redirect import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Activities from './pages/AthleteActivities';
 import ActivityList from './components/ActivityList';
-import Redirect from './utils/redirect';
+import Redirect from './utils/Redirect';
 import Login from './pages/Login';
 import ActivitiesMap from './pages/ActivitiesMap';
 import ActivitiesCard from './components/ActivitiesCard';
