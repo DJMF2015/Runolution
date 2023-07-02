0d8d984b8a54509e6279ba3be6c565b0a3df9e79
DJMF2015 <djm.fulton@gmail.com>
2023-07-02 17:42:38 +0100

    remove unused styled login button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,24 +34,3 @@ const StyledLoginContainer = styled.main`
   margin: 0 auto;
   height: 100vh;
 `;
-
-const StyledLoginButton = styled.a`
-  display: inline-block;
-  min-width: 80px;
-  background-color: #fc5200;
-  height: 48px;
-  width: 6%;
-  text-align: center;
-  margin-top: -7em;
-  line-height: 48px;
-  padding: 6px 12px;
-  font-size: 12px;
-  border-radius: 4px;
-  color: #fff;
-  font-weight: 700;
-  &:hover,
-  &:focus {
-    text-decoration: none;
-    filter: brightness(1.1);
-  }
-`;
