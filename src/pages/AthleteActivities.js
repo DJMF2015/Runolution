f396d623c4d67f2acf3797cb57a37d7ba47b7381
DJMF2015 <djm.fulton@gmail.com>
2023-07-03 12:01:32 +0100

    removed redundant styles import

--- src/pages/AthleteActivities.js
+++ src/pages/AthleteActivities.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import '../styles/AthleteStyles.css';
 import { getAthleteActivities, getUsersDetails } from '../utils/functions';
 import { useGetWindowWidth, useScroll } from '../utils/hooks';
 import { catchErrors, checkIfTokenExpired } from '../utils/helpers';
