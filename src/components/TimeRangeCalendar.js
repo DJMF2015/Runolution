0e069d56216a7099c929ff50f35ddd94dce3ce45
DJMF2015 <djm.fulton@gmail.com>
2023-08-26 18:52:27 +0100

    add breakdown chart and styles

--- src/components/TimeRangeCalendar.js
+++ src/components/TimeRangeCalendar.js
@@ -2,7 +2,7 @@ import { ResponsiveTimeRange } from '@nivo/calendar';
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import { formattedDate, getCurrentDate } from '../utils/conversion';
-
+import BreakdownChart from './BreakdownChart';
 const TimeRangeCalendar = (props) => {
   const accessToken = localStorage.getItem('access_token');
   let formatted = getCurrentDate();
@@ -42,41 +42,46 @@ const TimeRangeCalendar = (props) => {
 
   return (
     accessToken && (
-      <StyledCalendar>
-        *{' '}
-        <h4 style={{ marginBottom: '-1rem', marginTop: '1rem', textAlign: 'center' }}>
-          Activities
-        </h4>
-        <ResponsiveTimeRange
-          data={Object.keys(activitiesCount).map((date) => ({
-            day: date,
-            value: activitiesCount[date],
-          }))}
-          from={formattedSixtyDaysAgo}
-          to={date}
-          minValue={0}
-          emptyColor="#eeeeee"
-          colors={['#61cdbb', 'orange', '#e8c1a0', '#f47560']}
-          margin={{ top: 40, right: 40, bottom: 10, left: 30 }}
-          dayBorderWidth={2}
-          dayBorderColor="#ffffff"
-          legends={[
-            {
-              anchor: 'bottom-right',
-              direction: 'row',
-              justify: false,
-              itemCount: 5,
-              itemWidth: 50,
-              itemHeight: 50,
-              itemsSpacing: 12,
-              itemDirection: 'right-to-left',
-              translateX: -60,
-              translateY: -150,
-              symbolSize: 10,
-            },
-          ]}
-        />
-      </StyledCalendar>
+      <>
+        <StyledCalendar>
+          *{' '}
+          <h4 style={{ marginBottom: '-1rem', marginTop: '1rem', textAlign: 'center' }}>
+            Activities
+          </h4>
+          <ResponsiveTimeRange
+            data={Object.keys(activitiesCount).map((date) => ({
+              day: date,
+              value: activitiesCount[date],
+            }))}
+            from={formattedSixtyDaysAgo}
+            to={date}
+            minValue={0}
+            emptyColor="#eeeeee"
+            colors={['#61cdbb', 'orange', '#e8c1a0', '#f47560']}
+            margin={{ top: 40, right: 40, bottom: 10, left: 30 }}
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'row',
+                justify: false,
+                itemCount: 5,
+                itemWidth: 50,
+                itemHeight: 50,
+                itemsSpacing: 12,
+                itemDirection: 'right-to-left',
+                translateX: -60,
+                translateY: -150,
+                symbolSize: 10,
+              },
+            ]}
+          />
+        </StyledCalendar>
+        <StyledChartWrapper>
+          <BreakdownChart />
+        </StyledChartWrapper>
+      </>
     )
   );
 };
@@ -84,13 +89,10 @@ const TimeRangeCalendar = (props) => {
 export default TimeRangeCalendar;
 
 const StyledCalendar = styled.div`
-  /* display: flex; */
-  /* grid-template-rows: 1fr; */
   height: 35vh;
   width: 50vw;
   margin-left: 25rem;
   margin-bottom: -5rem;
-  border: 1px solid #e6e6e6;
   box-shadow: 0px 0 5px #e6e6e6;
 
   @media screen and (max-width: 1000px) {
@@ -104,9 +106,33 @@ const StyledCalendar = styled.div`
     height: 35vh;
     width: 50vw;
     margin-bottom: -11rem;
-    /* margin-left: 10rem; */
     border: 1px solid #e6e6e6;
     box-shadow: 0px 0 5px #e6e6e6;
+  }
+
+  @media screen and (max-width: 850px) {
+    display: none;
+  }
+`;
+
+const StyledChartWrapper = styled.div`
+  margin-left: 90rem;
+  margin-top: -18rem;
+  @media screen and (max-width: 1400px) {
+    margin-left: 88rem;
+    margin-top: -18rem;
+  }
+  @media screen and (max-width: 1150px) {
+    margin-left: 30rem;
+    margin-top: 5rem;
+  }
+  @media screen and (min-width: 980px) and (max-width: 1150px) {
+    margin-top: 6rem;
+    margin-left: 35rem;
+  }
+  @media screen and (min-width: 850px) and (max-width: 980px) {
+    margin-left: 17rem;
+    margin-top: 12rem;
   }
 
   @media screen and (max-width: 850px) {
