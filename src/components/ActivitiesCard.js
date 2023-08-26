9f4cd58bc6455b2c62ac3f7ded7ebbc844b7b783
DJMF2015 <djm.fulton@gmail.com>
2023-08-26 10:34:21 +0100

    removed commented out code

--- src/components/ActivitiesCard.js
+++ src/components/ActivitiesCard.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { ArrowUpCircleFill } from '@styled-icons/bootstrap/ArrowUpCircleFill';
-import { Mountain } from '@styled-icons/fa-solid/Mountain';
-import { Ruler } from '@styled-icons/fa-solid/Ruler';
 import styled from 'styled-components';
 import { useScroll } from '../utils/hooks';
 import MapCoordinatesHelper from '../utils/mapCoordinates';
@@ -29,10 +27,6 @@ export default function ActivitiesCard() {
       detailedActivity: [],
     },
   ]);
-  // const [queryData, setQueryData] = useState({
-  //   distance: [],
-  //   elevation: [],
-  // });
 
   const location = useLocation();
   const { from } = location.state;
@@ -84,8 +78,6 @@ export default function ActivitiesCard() {
       style: selectedLayer,
       antialias: true, // create the gl context with MSAA antialiasing, so custom layers are antialiased
       ...endLocation,
-      // zoom - zoom in closer when the route is only 5kms long or less and if not zoom to 10
-      // zoom: from.distance < 10000 ? 12.5 : 11.5,
       zoom: 1.5,
       pitch: 55,
       bearing: 60,
@@ -179,70 +171,6 @@ export default function ActivitiesCard() {
         .addTo(map)
         .togglePopup();
       map.addSource('trace', { type: 'geojson', data: data });
-
-      //   const animateMarker = () => {
-      //     // The total animation duration, in milliseconds
-      //     const animationDuration = 12000;
-      //     const path = turf.lineString(pinRoute);
-      //     // Get the total line distance
-      //     const pathDistance = turf.lineDistance(path);
-      //     let start;
-      //     function frame(time) {
-      //       if (!start) start = time;
-      //       const animationPhase = (time - start) / animationDuration;
-      //       if (animationPhase > 1) {
-      //         return;
-      //       }
-
-      //       // Get the new latitude and longitude by sampling along the path
-      //       const alongPath = turf.along(path, pathDistance * animationPhase).geometry
-      //         .coordinates;
-      //       const lngLat = {
-      //         lng: alongPath[0],
-      //         lat: alongPath[1],
-      //       };
-      //       // get the current distance along the path from the total length of the route
-      //       const currentDistance = pathDistance * animationPhase;
-      //       const distance = currentDistance.toFixed(2);
-      //       // Sample the terrain elevation. We rou nd to an integer value to
-      //       // prevent showing a lot of digits during the animation
-      //       const elevation = Math.floor(
-      //         // Do not use terrain exaggeration to get actual meter values
-      //         map.queryTerrainElevation(lngLat, { exaggerated: false })
-      //       );
-
-      //       setQueryData({ elevation, distance });
-
-      //       // Update the popup altitude value and marker location
-      //       popup.setHTML(from.name);
-      //       marker.setLngLat(lngLat);
-
-      //       // Rotate the camera at a slightly lower speed to give some parallax effect in the background
-      //       const rotation = 150 - animationPhase * pathDistance * 4;
-      //       map.setBearing(rotation % 360);
-      //       window.requestAnimationFrame(frame);
-      //     }
-      //     map.fitBounds([
-      //       // southwestern corner of the bounds
-      //       [data.geometry.coordinates[0][0], data.geometry.coordinates[0][1]],
-      //       // northeastern corner of the bounds
-      //       [data.geometry.coordinates[1][0], data.geometry.coordinates[1][1]],
-      //     ]);
-      //     // //zoom to bounds when animation completes
-      //     map.once('idle', function () {
-      //       map.fitBounds(data.geometry.coordinates, {
-      //         zoom: 14,
-      //         pitch: 65,
-      //         bearing: 200,
-      //         duration: 1000,
-      //         rotate: 200 * (Math.PI / 180), //start rotation of animation
-      //         //end location of animation
-      //         center: pinRoute[pinRoute.length - 1],
-      //       });
-      //     });
-      //     window.requestAnimationFrame(frame);
-      //   };
-      //   // animateMarker();
     });
 
     function rotateAndFlyTo(endLocation) {
@@ -253,9 +181,9 @@ export default function ActivitiesCard() {
       function animate(timestamp) {
         if (!start) start = timestamp;
         var progress = timestamp - start;
-        map.rotateTo((bearing + (360 * progress) / 3000) % 360, { duration: 0 });
+        map.rotateTo((bearing + (360 * progress) / 2000) % 360, { duration: 0 });
 
-        if (progress < 3000) {
+        if (progress < 2000) {
           // Continue rotating
           requestAnimationFrame(animate);
         } else {
@@ -388,62 +316,7 @@ export default function ActivitiesCard() {
             height: '100vh',
             position: 'relative',
           }}
-        >
-          {/* <div style={{ display: 'flex', alignItems: 'center' }}>
-            <MountainIcon
-              style={{
-                marginRight: '10px',
-                color:
-                  selectedLayer === 'mapbox://styles/mapbox/outdoors-v11'
-                    ? 'black'
-                    : 'white',
-              }}
-            />
-            <h1
-              style={{
-                position: 'relative',
-                display: 'inline-block',
-                marginTop: '1rem',
-                color:
-                  selectedLayer === 'mapbox://styles/mapbox/outdoors-v11'
-                    ? 'black'
-                    : 'white',
-                fontSize: '1.5rem',
-                zIndex: '1000',
-              }}
-            >
-              {queryData.elevation} mtrs
-            </h1>
-          </div> */}
-
-          {/* <div style={{ display: 'inline', alignItems: 'center' }}>
-            <RulerIcon
-              style={{
-                marginRight: '10px',
-                color:
-                  selectedLayer === 'mapbox://styles/mapbox/outdoors-v11'
-                    ? 'black'
-                    : 'white',
-              }}
-            />
-            <h1
-              style={{
-                position: 'relative',
-                display: 'inline-block',
-                marginTop: '1rem',
-                color:
-                  selectedLayer === 'mapbox://styles/mapbox/outdoors-v11'
-                    ? 'black'
-                    : 'white',
-                fontSize: '1.5rem',
-                zIndex: '1000',
-              }}
-            >
-              {' '}
-              {queryData.distance} {' km'}
-            </h1>
-          </div> */}
-        </div>
+        ></div>
       </div>
     </>
   );
@@ -470,24 +343,7 @@ const Text = styled.div`
     text-align: left;
   }
 `;
-const MountainIcon = styled(Mountain)`
-  position: relative;
-  color: ${(props) => props.theme.colour.white};
-  z-index: 1000;
-  margin-left: 50vw;
-  color: black;
-  width: 1.5rem;
-  height: 1.5rem;
-`;
 
-const RulerIcon = styled(Ruler)`
-  position: relative;
-  z-index: 1200;
-  margin-left: 50vw;
-  color: black;
-  width: 1.5rem;
-  height: 1.5rem;
-`;
 const LinkText = styled.div`
   font-family: Arial, Helvetica, sans-serif;
   color: 'white';
