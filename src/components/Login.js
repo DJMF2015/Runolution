5d957ab3650fc92b6cc6cafe5edaff1d5b515c51
DJMF2015 <djm.fulton@gmail.com>
2023-09-08 14:20:36 +0100

    add http uri!

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { client_id } from '../utils/config';
 const Login = () => {
   const [logout, setLoggedOut] = useState(true);
-  const redirectUrl = 'strava-personal-dashboard.vercel.app/redirect';
+  const redirectUrl = 'http://strava-personal-dashboard.vercel.app/redirect';
 
   const handleLogin = () => {
     localStorage.clear();
