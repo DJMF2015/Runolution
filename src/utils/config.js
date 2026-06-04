cd00e80d400b3cb5d6f24fe54edefe7575c69a2b
DJMF2015 <djm.fulton@gmail.com>
2026-06-04 19:20:35 +0100

    add base link for revoking authorisation of app

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,8 @@
 const auth_link = 'https://www.strava.com/oauth/token';
+const auth_revoke_link = 'https://www.strava.com/oauth/revoke';
 const baseURL = 'https://www.strava.com/api/v3';
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const client_secret = process.env.REACT_APP_CLIENT_SECRET;
 const refresh_token = process.env.REACT_APP_REFRESH_SECRET;
 
-export { auth_link, baseURL, client_id, client_secret, refresh_token };
+export { auth_link, auth_revoke_link, baseURL, client_id, client_secret, refresh_token };
