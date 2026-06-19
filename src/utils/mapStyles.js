2c9fb3619a847bd885d2603e09c53529f9a53250
DJMF2015 <djm.fulton@gmail.com>
2026-06-19 18:49:43 +0100

    switch satellite map to hold no labels

--- src/utils/mapStyles.js
+++ src/utils/mapStyles.js
@@ -1,4 +1,4 @@
 export const ACTIVITY_DETAIL_MAP_STYLES = {
   street: 'mapbox://styles/mapbox/outdoors-v12',
-  satellite: 'mapbox://styles/mapbox/satellite-streets-v12',
+  satellite: 'mapbox://styles/mapbox/satellite-v9',
 };
